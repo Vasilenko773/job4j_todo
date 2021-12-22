7715375a3e1795ab42bd94b3914b88ae57cc8ea2
Нина <vasilenko.lesnik2017@yandex.ru>
2021-12-22 21:57:28 +0900

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/store/HibernateUtil.java
+++ src/main/java/ru/job4j/store/HibernateUtil.java
@@ -2,13 +2,14 @@ package ru.job4j.store;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.model.Item;
-import org.hibernate.query.Query;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class HibernateUtil implements Store {
 
@@ -32,64 +33,52 @@ public class HibernateUtil implements Store {
 
     @Override
     public List<Item> findAllItems() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List<Item> result = session.createQuery("from ru.job4j.model.Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(session -> session.createQuery("from ru.job4j.model.Item").list());
     }
 
     @Override
     public Item save(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return (Item) this.tx(session -> session.save(item));
+
     }
 
     @Override
     public Item findById(Integer id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item result = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(session -> session.get(Item.class, id));
     }
 
     @Override
     public void update(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.createQuery("UPDATE ru.job4j.model.Item set done = :done where id = :id")
+        this.tx(session -> session.createQuery("UPDATE ru.job4j.model.Item set done = :done where id = :id")
                 .setParameter("done", true)
                 .setParameter("id", id)
-                .executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+                .executeUpdate());
     }
 
     @Override
     public Item findByDescription(String description) {
-        Session session = sf.openSession();
-        Item result = session.get(Item.class, description);
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(session -> session.get(Item.class, description));
     }
 
     @Override
     public List<Item> findByDone() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Query query = session.createQuery("from ru.job4j.model.Item where done = :done");
-        List<Item> result = query.setParameter("done", false).list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(session -> session.createQuery("from ru.job4j.model.Item where done = :done")
+                .setParameter("done", false).list());
     }
 
+    private <T> T tx(final Function<Session, T> command) {
+        Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
 }
+
